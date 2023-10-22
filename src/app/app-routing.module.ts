f127cb2cdb2b113f50495ef800f806e6ec7d6601
Gilles <sellig.blater@free.fr>
2023-10-22 14:22:59 +0200

    2 routes créées  et leur composants. Home et Not-Found.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: '**', component: NotFoundComponent, title: 'Not found' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
